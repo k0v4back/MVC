fcb34a1649e11e7044cb8353e4a3692ddc3ebf85
Vadim Kosolapov <vadkos33@outlook.com>
2018-09-08 19:49:22 +0200

    Clearing the code

--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,4 @@
 <?php
 return array (
-    
-//    '' => 'site/index',//Контроллер Site, и actionIndex
-    
-
-    //Из news/sport/114 вырезать sport/114 и подставить на места ссылок $1, $2
-    'news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2', //Для того чтобы работали ссылкенужно их в роутере описать
-    'news/([0-9]+)' => 'news/view',
     '' => 'test/index',
-    //'news' => 'news/index', //news - это то что вводится пользователем url
-    //'products' => 'product/list', //actionList в ProductController
 );
